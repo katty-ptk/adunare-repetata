3fd8f276e2944ed51887cd4a37decec7d8aca00d
Katty Lazar <katy.lazar05@gmail.com>
2020-07-31 11:46:37 +0300

    added project

---
+++ app.js
@@ -0,0 +1,33 @@
+// x = a + a + a + ... + a (b ori) // 
+
+$("#submit-btn").click(function (event) {
+    event.preventDefault();
+
+    $first_num = parseInt($("#first_Num").val());
+    $second_num = parseInt($("#second_Num").val());
+
+    // input error checking
+    if (isNaN($first_num) || isNaN($second_num)) {
+        $input_error_text = '<p id="error-text">Te rog introdu numere. :)</p>';
+        $("#input-error-text-p").html($input_error_text);
+        return false;
+    }
+
+    $i = 0;
+    $x = 0;
+
+    while ($i < $second_num) {
+        // $first_num += parseInt($first_num);
+        $x += $first_num; // x = 0 + a de b ori
+        $i++;
+    }
+
+    $result = document.createElement('p');
+    $result.textContent = "x = " + $x;
+    $("#result").html($result);
+
+    console.log($x);
+
+  //  alert($first_num);
+ //   alert('working');
+});
