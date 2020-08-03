6ccb44b42a4b0c11dc9765f6ddf5f55b5d9ab40e
Katty Lazar <katy.lazar05@gmail.com>
2020-08-03 10:27:37 +0300

    fixed result messages, but function working weird

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
+$i = 1;
+$x = 0;
+$result_p = document.createElement('p');
 
-$("#submit-btn").click(function ( event ) {
+$("#submit-btn").click( function ( event ) {
     event.preventDefault();
 
     $first_num = parseInt($("#first_Num").val());
     $second_num = parseInt($("#second_Num").val());
 
-    if ( isNaN ( $first_num ) || isNaN ( $second_num ) ) {
-        
-        $result = '<p class="error" id="input-error">Te rog introdu numere. :)</p>';
-        $(".result").html($result);
-    
-    } else {
-
-        $i = 0;
-        $x = 0;
-
-        while ( $i < $second_num ) {
-
+    if ( !( isNaN ( $first_num ) ) && !( isNaN ( $second_num ) ) ) {
+        do {
+            $i += 1;
             $x += $first_num;
-            $i++;
 
-            $result_p = document.createElement('p');
             $result_p.textContent = "x = " + $x;
             $("#result").html($result_p);
-            
-        }
 
+
+
+            console.log($x);
+        } while ( $i <= $second_num );
+
+       
+    } else  {
+        $result_p.textContent = "Te rog introdu numere. :)";
+        $(".result").html( $result_p );
     }
-
-
-    // alert($first_num + $second_num)
-
 });
