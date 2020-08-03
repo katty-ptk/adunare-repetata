7444449c18726b88cf7a86f59ebc041fc271d1cc
Katty Lazar <katy.lazar05@gmail.com>
2020-08-03 11:02:25 +0300

    fixed bug

--- app.js
+++ app.js
@@ -1,38 +1,35 @@
-// x = a + a + a + ... + a (b ori) // 
+$i = 1;
+$x = 0;
+$result_p = document.createElement('p');
 
-$("#submit-btn").click(function (event) {
+$("#submit-btn").click( function ( event ) {
     event.preventDefault();
 
     $first_num = parseInt($("#first_Num").val());
     $second_num = parseInt($("#second_Num").val());
 
-    // input error checking
-    if (isNaN($first_num) || isNaN($second_num)) {
-        $input_error_text = '<p id="error-text">Te rog introdu numere. :)</p>';
-        $("#input-error-text-p").html($input_error_text);
-        return false;
+    if ( !( isNaN ( $first_num ) ) && !( isNaN ( $second_num ) ) ) {
+        do {
+            $i += 1;
+            $x += $first_num;
+
+            $result_p.textContent = "x = " + $x;
+            $("#result").html($result_p);
+            
+           // console.log($x);
+            
+        } while ( $i <= $second_num );
+
+    } else  {
+        
+        $result_p.textContent = "Te rog introdu numere. :)";
+        $("#result").html( $result_p );
+        
     }
-
-    // default values for i and x
-    $i = 0;
-    $x = 0;
-
-    while ($i < $second_num) {
-        // $first_num += parseInt($first_num);
-        $x += $first_num; // x = 0 + a de b ori
-        $i++;
-    }
-
-    // append the result to html without using append :DD
-    $result = document.createElement('p');
-    $result.textContent = "x = " + $x;
-    $("#result").html($result);
 
     $("#first_Num").val("");
     $("#second_Num").val("");
 
-    console.log($x);
+//    console.log($x);
 
-  //  alert($first_num);
- //   alert('working');
 });
