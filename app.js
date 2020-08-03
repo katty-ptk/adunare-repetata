3927188a8677094c7efbb3e011feaf6471ed7aa8
Katty Lazar <katy.lazar05@gmail.com>
2020-08-03 09:44:09 +0300

    not working

--- app.js
+++ app.js
@@ -1,42 +1,34 @@
-// x = a + a + a + ... + a (b ori) // 
 
-// STOP SHOWING ERROR IF THE INPUTS ARE FILLED IN CORRECTLY // 
-
-$("#submit-btn").click(function (event) {
+$("#submit-btn").click(function ( event ) {
     event.preventDefault();
 
     $first_num = parseInt($("#first_Num").val());
     $second_num = parseInt($("#second_Num").val());
 
-    // input error checking
-    if (isNaN($first_num) || isNaN($second_num)) {
-        $input_error_text = '<p id="error-text">Te rog introdu numere. :)</p>';
-        $("#input-error-text-p").html($input_error_text);
-        return false;
-    }
+    if ( isNaN ( $first_num ) || isNaN ( $second_num ) ) {
+        
+        $result = '<p class="error" id="input-error">Te rog introdu numere. :)</p>';
+        $(".result").html($result);
+    
+    } else {
 
+        $i = 0;
+        $x = 0;
 
+        while ( $i < $second_num ) {
 
-    // default values for i and x
-    $i = 0;
-    $x = 0;
+            $x += $first_num;
+            $i++;
 
-    while ($i < $second_num) {
-
-        // $first_num += parseInt($first_num);
-        $x += $first_num; // x = 0 + a de b ori
-        $i++;
-
-       // append the result to html without using append :DD
-        $result = document.createElement('p');
-        $result.textContent = "x = " + $x;
-        $("#result").html($result);
-
-        if ($input_error_text){
-            $($input_error_text).style = 'visibility: hidden';
+            $result_p = document.createElement('p');
+            $result_p.textContent = "x = " + $x;
+            $("#result").html($result_p);
+            
         }
 
     }
 
-    console.log($x);
+
+    // alert($first_num + $second_num)
+
 });
